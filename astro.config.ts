ee5a9e8c88ff881d1b2240e75ab9f2f1654e89c4
Victor Sokovnin <v@sokovnin.me>
2026-01-23 11:16:51 +0300

    Restore base: '/blog' to fix category URLs

--- astro.config.ts
+++ astro.config.ts
@@ -24,6 +24,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 export default defineConfig({
   output: 'static',
   site: 'https://sokovnin.me',
+  base: '/blog',
 
   integrations: [
     tailwind({
